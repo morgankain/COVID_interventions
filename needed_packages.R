d12a161d18c8d607164930c2ea3a0af5db4af43c
morgankain <kainm@mcmaster.ca>
2020-06-15 13:33:28 -0700

    small function for capital letters for ggplot

---
+++ needed_packages.R
@@ -0,0 +1,25 @@
+## Required packages to run this code
+needed_packages <- c(
+    "pomp"
+  , "plyr"
+  , "dplyr"
+  , "ggplot2"
+  , "magrittr"
+  , "scales"
+  , "lubridate"
+  , "tidyr"
+  , "foreach"
+  , "doParallel"
+  , "data.table"
+  , "doRNG"
+  , "scales")
+
+## load packages. Install all packages that return "FALSE"
+lapply(needed_packages, require, character.only = TRUE)
+
+simpleCap <- function(x) {
+    s <- strsplit(x, " ")[[1]]
+    paste(toupper(substring(s, 1, 1)), substring(s, 2),
+          sep = "", collapse = " ")
+}
+
