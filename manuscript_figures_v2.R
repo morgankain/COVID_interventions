a190354e044c2104bed118204475207c4c8df069
morgankain <kainm@mcmaster.ca>
2020-06-25 11:37:41 -0700

    tweaks to figure 1

--- manuscript_figures_v2.R
+++ manuscript_figures_v2.R
@@ -2,14 +2,14 @@ source("needed_packages.R")
 source("ggplot_theme.R")
 
 counties_list = {list(
-  SC = list(rds.name = "output/Santa Clara_0_2020-06-24_cont_final.Rds"),
-  LA = list(rds.name = "output/Los Angeles_0_2020-06-24_cont_final.Rds"),
-  KC = list(rds.name = "output/King_0_2020-06-24_cont_final.Rds")
-  # FC = list(rds.name = "output/Fulton_0_2020-06-24_cont_round2.Rds"),
-  # MD = list(rds.name = "output/Miami-Dade_0_2020-06-24_cont_round2.Rds"),
-  # MC = list(rds.name = "output/Mercer_0_2020-06-24_cont_round2.Rds"),
-  # BC = list(rds.name = "output/Bucks_0_2020-06-24_cont_round2.Rds"),
-  # CC = list(rds.name = "output/Chester_0_2020-06-24_cont_round2.Rds")
+   SC = list(rds.name = "output/Santa Clara_0_2020-06-25_cont_round2.Rds")
+ , LA = list(rds.name = "output/Los Angeles_0_2020-06-25_cont_round2.Rds")
+ , KC = list(rds.name = "output/King_0_2020-06-25_cont_round2.Rds")
+ , FC = list(rds.name = "output/Fulton_0_2020-06-25_cont_round2.Rds")
+ , MD = list(rds.name = "output/Miami-Dade_0_2020-06-25_cont_round2.Rds")
+#,  MC = list(rds.name = "output/Mercer_0_2020-06-25_cont_round2.Rds")
+#,  BC = list(rds.name = "output/Bucks_0_2020-06-25_cont_round2.Rds")
+#,  CC = list(rds.name = "output/Chester_0_2020-06-25_cont_round2.Rds")
 )}
 
 #####
@@ -25,13 +25,16 @@ int_vars <- list(
  , sim_title          = "Reality"                                        
 )
 
-sim_vars = list(loglik.max     =  T,
-                loglik.num     = 5,
-                ci.stoc        = 0.025,
-                ci.epidemic    = T,
-                nsim           = 500,
-                plot_vars      = c("cases", "deaths"),
-                plot.median    = T)
+sim_vars <- list(
+   loglik.max      = F
+ , loglik.num      = 3
+ , ci.stoc         = 0.025
+ , ci.epidemic     = T
+ , ci.epidemic_cut = 500
+ , nsim            = 500
+ , plot_vars       = c("cases", "deaths")
+ , plot.median     = F
+  )
 
 # run the simulations for all locations
 fig1_data <- plyr::ldply(counties_list, 
@@ -42,29 +45,33 @@ fig1_data <- plyr::ldply(counties_list,
           SEIR.sim.f.ci %<>% data.frame() %>%
           full_join(pomp_data %>%
                       arrange(day) %>%
-                      mutate(D = cumsum(ifelse(is.na(deaths), 0, deaths))*
+                      mutate(D = cumsum(ifelse(is.na(deaths), 0, deaths)) *
                                ifelse(is.na(deaths), NA, 1),
                              date = day + date_origin) %>%
                       select(date, any_of(plot_vars)) %>%
                       pivot_longer(any_of(plot_vars), values_to = "data")) %>%
             as.data.frame() %>%
-            rbind(., (Reff %>% mutate(name = "Reff", lwr = NA, mid = Reff, upr = NA, data = NA) %>% select(-Reff) %>% dplyr::select(date, paramset, everything()))) %>%
-            rbind(., (detect %>% mutate(name = "detect", lwr = NA, mid = detect, upr = NA, data = NA) %>% select(-detect) %>% dplyr::select(date, paramset, everything()))) %>%
+            rbind(., (Reff %>% mutate(name = "Reff", lwr = NA, mid = Reff, upr = NA, .groups = NA, data = NA) %>% select(-Reff) %>% dplyr::select(date, paramset, everything()))) %>%
+            rbind(., (detect %>% mutate(name = "detect", lwr = NA, mid = detect, upr = NA, .groups = NA, data = NA) %>% select(-detect) %>% dplyr::select(date, paramset, everything()))) %>%
             mutate(intervention = sim_title,
                  county = variable_params[1, "county"] %>% as.character,
-                 state = variable_params[1, "state"] %>% as.character)
+                 state  = variable_params[1, "state"] %>% as.character)
         return(SEIR.sim.f.ci)
 })}, .id = NULL)
 
 # plot the fits and data
-fig1_colors = c("#FF0000",
-                "#00A08A",
-                "#F2AD00",
-                "#F98400",
-                "#5BBCD6",
-                "black", "blue", "gold")#[c(1,4,5,2,3)]
+fig1_colors = c(
+  "#FF0000"
+, "#00A08A"
+, "#F2AD00"
+, "dodgerblue3"
+, "maroon3"
+, "#F98400"
+, "#5BBCD6"
+, "black"
+  )#[c(1,4,5,2,3)]
 
-# fig1_data$name <- sapply(fig1_data$name, simpleCap)
+fig1_data$name <- sapply(fig1_data$name, simpleCap)
 
 ## LA with same axis
 {
@@ -96,172 +103,8 @@ fig1_data %>%
   xlab("Date")
 }
 
-## LA with a different axis
-# {
-# fig1.1 <- {fig1_data %>% 
-#   filter(county != "Los Angeles") %>%
-#   mutate(county = paste0(county, " County, ", state)) %>%
-#   filter(date < as.Date("2020-06-08")) %>%
-#   filter(date >= as.Date("2020-02-10")) %>%
-#   filter(!(name %in% c("Reff", "Detect"))) %>%
-#   group_by(county) %>% 
-#   mutate(nparams = 0.5/length(unique(paramset))) %>% 
-#   ggplot(aes(x = date, y = mid, ymin = lwr, ymax = upr, 
-#              fill  = county, color = county, 
-#              group = interaction(county,paramset))) +
-#   geom_ribbon(aes(alpha = I(nparams)), colour = NA) +
-#   geom_line() +
-#   geom_point(aes(x = date, y = data), 
-#              color = "black", size = 0.75) + 
-#   scale_y_continuous(trans = "sqrt") + 
-#   scale_fill_manual(guide = F, values = fig1_colors) +
-#   scale_color_manual(guide = F, values = fig1_colors) +
-#   facet_grid(name ~ county, scales = "free_y", switch = "y")  +
-#   ylab("") + 
-#   theme(
-#     strip.background = element_blank()
-#     , strip.placement = "outside"
-#     , strip.text.x = element_text(size = 11)
-#     , strip.text.y = element_text(size = 16)
-#     , axis.text.x = element_text(size = 12)
-#     , axis.title.y = element_text(margin = margin(l = 1))
-#     , plot.margin = unit(c(0.25,0.25,0.25,0.25), "cm")
-#     ) + 
-#     xlab("") +
-#     labs(tag = "C.") +
-#     theme(plot.tag.position = "topleft"
-#           , plot.tag = element_text(face = "bold" 
-#                                    , margin = margin(b = -16, 
-#                                                      r = -45)))}
-# 
-# fig1.2 <- {fig1_data %>% 
-#   filter(county == "Los Angeles") %>%
-#   mutate(county = paste0(county, " County, ", state)) %>%
-#   filter(date < as.Date("2020-06-08")) %>%
-#   filter(date >= as.Date("2020-02-10")) %>%
-#   filter(!(name %in% c("Reff", "Detect"))) %>%
-#   group_by(county) %>% 
-#   mutate(nparams = 0.5/length(unique(paramset))) %>% 
-#   ggplot(aes(x = date, y = mid, ymin = lwr, ymax = upr, 
-#              fill  = county, color = county, 
-#              group = interaction(county,paramset))) +
-#   geom_ribbon(aes(alpha = I(nparams)), colour = NA) +
-#   geom_line() +
-#   geom_point(aes(x = date, y = data), 
-#              color = "black", size = 0.75) + 
-#   scale_y_continuous(trans = "sqrt", position = "right") + 
-#   scale_fill_manual(guide = F, values = fig1_colors[5]) +
-#   scale_color_manual(guide = F, values = fig1_colors[5]) +
-#   facet_grid(name ~ county, scales = "free_y", switch = "y")  +
-#   ylab("") + 
-#   theme(
-#     strip.background = element_blank()
-#     , strip.placement = "outside"
-#     , strip.text.y = element_blank()
-#     , strip.text.x = element_text(size = 11)
-#     , axis.text.x = element_text(size = 12)
-#     , plot.margin = unit(c(0.25,0.25,0.25,0), "cm")) +
-#   xlab("") 
-#   # xlab("Date")
-#   }
-# 
-# fig1.3 <- {fig1_data %>% 
-#   filter(date < as.Date("2020-06-08")) %>%
-#   filter(date >= as.Date("2020-02-10")) %>%
-#   filter(name == "Reff") %>%
-#   # ugly way to calculate 7 day smoothing 
-#   arrange(county, paramset, date) %>%
-#   group_by(county, paramset) %>%
-#   mutate(
-#     lag3 = lag(mid, 3),
-#     lag2 = lag(mid, 2),
-#     lag1 = lag(mid, 1),
-#     lead1 = lead(mid, 1),
-#     lead2 = lead(mid, 2),
-#     lead3 = lead(mid, 3)
-#   ) %>% 
-#   rowwise() %>% 
-#   mutate(mid = mean(c(lag1, lag2, lag3, mid, lead1, lead2, lead3), na.rm = T)) %>% 
-#   select(-starts_with("lag"), -starts_with("lead")) %>% 
-#   mutate(paramset = as.character(paramset)) %>%
-#   group_by(county, state, date) %>% 
-#   {rbind(., 
-#          dplyr::summarise(., 
-#                           med = mean(mid), 
-#                           lwr = min(mid),
-#                           upr = max(mid),
-#                           .groups = "drop") %>% 
-#            rename(mid = med) %>% 
-#            mutate(paramset = "summary",
-#                   name = "Detect",
-#                   intervention = "Reality",
-#                   data = NA))} %>% 
-#   mutate(width = ifelse(paramset == "summary", 1.5, 0.25),
-#          alpha = ifelse(paramset == "summary", 1, 0.2)) %>%
-#   ggplot(aes(x = date, y = mid, ymin = lwr, ymax = upr, 
-#              fill  = county, color = county, 
-#              group = interaction(county,paramset))) +
-#   geom_ribbon(alpha = 0.25, color = NA) +
-#   geom_line(aes(size = I(width), alpha = I(alpha))) +
-#   scale_color_manual(guide= F, values = fig1_colors[c(1, 2, 5, 3, 4)]) +
-#   scale_fill_manual(guide= F, values = fig1_colors[c(1, 2, 5, 3, 4)]) +
-#   geom_hline(yintercept  = 1, linetype = "dashed") + 
-#   ylab(expression("R"[e])) + 
-#   xlab("") +
-#   scale_y_continuous(position = "left") + 
-#   ggtitle("A. Reproduction number")+
-#   theme(plot.title.position = "plot",
-#         plot.title = element_text(face = "bold"))
-#   }
-# 
-# fig1.4 <-  {fig1_data %>%  
-#   filter(date < as.Date("2020-06-08")) %>%
-#   filter(date >= as.Date("2020-02-10")) %>%
-#   filter(name == "Detect") %>%
-#   mutate(paramset = as.character(paramset)) %>%
-#   group_by(county, state, date) %>% 
-#   {rbind(., 
-#          dplyr::summarise(., 
-#                           med = mean(mid), 
-#                           lwr = min(mid),
-#                           upr = max(mid),
-#                           .groups = "drop") %>% 
-#            rename(mid = med) %>% 
-#            mutate(paramset = "summary",
-#                   name = "Detect",
-#                   intervention = "Reality",
-#                   data = NA))} %>% 
-#   mutate(width = ifelse(paramset == "summary", 1.5, 0.25),
-#          alpha = ifelse(paramset == "summary", 1, 0.2)) %>%
-#   ggplot(aes(x = date, y = mid, ymin = lwr, ymax = upr, 
-#              group = interaction(county, paramset),
-#              fill  = county, color = county)) +
-#   geom_ribbon(alpha = 0.25, color = NA) +
-#   geom_line(aes(size = I(width), alpha = I(alpha))) +
-#   scale_color_manual(guide= F, values = fig1_colors[c(1, 2, 5, 3, 4)]) +
-#   scale_fill_manual(guide= F, values = fig1_colors[c(1, 2, 5, 3, 4)]) +
-#   # geom_hline(yintercept  = 1, linetype = "dashed") + 
-#   ylab("Detection probability") + 
-#   xlab("") + 
-#   scale_y_continuous(labels = scales::percent_format(accuracy = 1),
-#                      position = "right") +
-#   ggtitle("B. Syptomatic detection")+
-#   theme(plot.title.position = "plot",
-#         plot.title = element_text(face = "bold"))
-#   }
-#   
-# # gridExtra::grid.arrange(fig1.1, fig1.2, ncol = 2, widths = c(4, 1.2))
-# fig1 <- gridExtra::arrangeGrob(fig1.1, fig1.2, fig1.3,fig1.4,  
-#                        layout_matrix = matrix(c(3, 4, 4, 1, 1, 2), 
-#                                               byrow  = T, nrow = 2),
-#                        widths = c(2.6, 1.3, 1.3), heights = c(1.5, 2.5)) 
-# gridExtra::grid.arrange(fig1)
-# # ggsave("figures/Manuscript2/Figure1.pdf", 
-# #        fig1, 
-# #        device = "pdf",
-# #        width = 12,
-# #        height = 8)
-# }
+## LA with different axis
+source("manuscript_figures_v2_1b.R")
 
 #####
 ## Figure 2: Interventions ----
@@ -270,77 +113,78 @@ fig1_data %>%
 int_vars <- {
 list(
   lift_all = list(
-  counter.factual    = FALSE,
-  int.movement       = c("post", "pre"),
-  int.type           = "none",
-  int.init           = "2020-07-01",
-  sim_end            = "2020-07-31",
-  sim_title          = "Lift"                                    
+  counter.factual    = FALSE
+, int.movement       = c("post", "post")
+, int.type           = "none"
+, int.init           = "2020-07-01"
+, sim_end            = "2020-07-31"
+, sim_title          = "Lift"                                    
 ),
   continue_SIP = list(
-  counter.factual    = FALSE,
-  int.movement       = c("post", "post"),
-  int.type           = "none",
-  int.init           = "2020-07-01",
-  sim_end            = "2020-07-31",
-  sim_title          = "Continue SIP"                                        
+  counter.factual    = FALSE
+, int.movement       = c("post", "pre")
+, int.type           = "none"
+, int.init           = "2020-07-01"
+, sim_end            = "2020-07-31"
+, sim_title          = "Continue SIP"                                        
 ),
   inf_iso = list(
-  counter.factual    = FALSE,
-  int.movement       = c("post", "mid"),
-  int.type           = "inf_iso",
-  int.init           = "2020-07-01",
-  sim_end            = "2020-07-31",
-  sim_title          = "Isolate",
-  iso_mild_level     = 0.1,
-  iso_severe_level   = 0.1
+  counter.factual    = FALSE
+, int.movement       = c("post", "mid")
+, int.type           = "inf_iso"
+, int.init           = "2020-07-01"
+, sim_end            = "2020-07-31"
+, sim_title          = "Isolate"
+, iso_mild_level     = 0.1
+, iso_severe_level   = 0.1
 ),
   tail_chop1 = list(
-  counter.factual    = FALSE,
-  int.movement       = c("post", "mid"),
-  int.type           = "tail",
-  int.init           = "2020-07-01",
-  sim_end            = "2020-07-31",
-  sim_title          = "Superspreading_0.01_1",
-  int.beta_catch     = 0.01,
-  int.beta_catch_type= "pct",
-  int.catch_eff      = 1.0
+  counter.factual     = FALSE
+, int.movement        = c("post", "mid")
+, int.type            = "tail"
+, int.init            = "2020-07-01"
+, sim_end             = "2020-07-31"
+, sim_title           = "Superspreading_0.01_1"
+, int.beta_catch      = 0.01
+, int.beta_catch_type = "pct"
+, int.catch_eff       = 1.0
 ),
   tail_chop2 = list(
-  counter.factual    = FALSE,
-  int.movement       = c("post", "mid"),
-  int.type           = "tail",
-  int.init           = "2020-07-01",
-  sim_end            = "2020-07-31",
-  sim_title          = "Superspreading_0.02_1",
-  int.beta_catch     = 0.02,
-  int.beta_catch_type= "pct",
-  int.catch_eff      = 1.0
+  counter.factual     = FALSE
+, int.movement        = c("post", "mid")
+, int.type            = "tail"
+, int.init            = "2020-07-01"
+, sim_end             = "2020-07-31"
+, sim_title           = "Superspreading_0.02_1"
+, int.beta_catch      = 0.02
+, int.beta_catch_type = "pct"
+, int.catch_eff       = 1.0
 ),
   tail_chop3 = list(
-  counter.factual    = FALSE,
-  int.movement       = c("post", "mid"),
-  int.type           = "tail",
-  int.init           = "2020-07-01",
-  sim_end            = "2020-07-31",
-  sim_title          = "Superspreading_0.05_1",
-  int.beta_catch     = 0.05,
-  int.beta_catch_type= "pct",
-  int.catch_eff      = 1.0
+  counter.factual     = FALSE
+, int.movement        = c("post", "mid")
+, int.type            = "tail"
+, int.init            = "2020-07-01"
+, sim_end             = "2020-07-31"
+, sim_title           = "Superspreading_0.05_1"
+, int.beta_catch      = 0.05
+, int.beta_catch_type = "pct"
+, int.catch_eff       = 1.0
 )
 )
 }
 
-
-sim_vars = list(
-loglik.max     = TRUE,
-plot_vars      = c("cases", "deaths"),
-ci.stoc        = 0.025,
-ci.epidemic    = T,
-nsim           = 200)
+sim_vars <- list(
+ loglik.max       = TRUE
+, plot_vars       = c("cases", "deaths")
+, ci.stoc         = 0.025
+, ci.epidemic     = T
+, ci.epidemic_cut = 500
+, nsim            = 200
+  )
 
 fig2_data <- ldply(int_vars, function(j) {
-  ldply(counties_list[c("SC", "KC")], function(i) {
+  ldply(counties_list[c("KC", "MD")], function (i) {
     source("COVID_simulate_cont_params.R", local = T)
     with(c(i, j, sim_vars), {
       print(int.beta_catch)
---
+++ manuscript_figures_v2_1b.R
@@ -0,0 +1,137 @@
+fig1_data$county <- factor(fig1_data$county)
+{
+ fig1.1 <- {fig1_data %>% 
+   filter(county != "Los Angeles") %>%
+   mutate(county = paste0(county, " County, ", state)) %>%
+   filter(date < as.Date("2020-06-08")) %>%
+   filter(date >= as.Date("2020-02-10")) %>%
+   filter(!(name %in% c("Reff", "Detect"))) %>%
+   group_by(county) %>% 
+   mutate(nparams = 0.5/length(unique(paramset))) %>% 
+   ggplot(aes(x = date, y = mid, ymin = lwr, ymax = upr, 
+              fill  = county, color = county, 
+              group = interaction(county, paramset))) +
+   geom_ribbon(aes(alpha = I(nparams)), colour = NA) +
+   geom_line() +
+   geom_point(aes(x = date, y = data), 
+              color = "black", size = 0.75) + 
+   scale_y_continuous(trans = "sqrt") + 
+   scale_fill_manual(guide = F, values = fig1_colors) +
+   scale_color_manual(guide = F, values = fig1_colors) +
+   facet_grid(name ~ county, scales = "free_y", switch = "y")  +
+   ylab("") + 
+   theme(
+     strip.background = element_blank()
+     , strip.placement = "outside"
+     , strip.text.x = element_text(size = 10)
+     , strip.text.y = element_text(size = 16)
+     , axis.text.x = element_text(size = 12)
+     , axis.title.y = element_text(margin = margin(l = 1))
+     , plot.margin = unit(c(0.25,0.25,0.25,0.25), "cm")
+     ) + 
+     xlab("") #+
+ #    labs(tag = "A.") +
+  #   theme(plot.tag.position = "topleft"
+  #         , plot.tag = element_text(face = "bold" 
+  #                                  , margin = margin(b = -16, 
+  #                                                    r = -35)))
+   }
+ 
+ fig1.2 <- {fig1_data %>% 
+   arrange(county, paramset, date) %>%
+   filter(county == "Los Angeles") %>%
+   mutate(county = paste0(county, " County, ", state)) %>%
+   filter(date < as.Date("2020-06-08")) %>%
+   filter(date >= as.Date("2020-02-10")) %>%
+   filter(!(name %in% c("Reff", "Detect"))) %>%
+   group_by(county) %>% 
+   mutate(nparams = 0.5/length(unique(paramset))) %>% 
+   ggplot(aes(x = date, y = mid, ymin = lwr, ymax = upr, 
+              fill  = county, color = county, 
+              group = interaction(county,paramset))) +
+   geom_ribbon(aes(alpha = I(nparams)), colour = NA) +
+   geom_line() +
+   geom_point(aes(x = date, y = data), 
+              color = "black", size = 0.75) + 
+   scale_y_continuous(trans = "sqrt", position = "right") + 
+   scale_fill_manual(guide = F, values = fig1_colors[5]) +
+   scale_color_manual(guide = F, values = fig1_colors[5]) +
+   facet_grid(name ~ county, scales = "free_y", switch = "y")  +
+   ylab("") + 
+   theme(
+     strip.background = element_blank()
+     , strip.placement = "outside"
+     , strip.text.y = element_blank()
+     , strip.text.x = element_text(size = 11)
+     , axis.text.x = element_text(size = 12)
+     , plot.margin = unit(c(0.20,0.25,0.25,0), "cm")) +
+    xlab("")
+   }
+ 
+ fig1.3 <- {fig1_data %>% 
+   filter(date < as.Date("2020-06-08")) %>%
+   filter(date >= as.Date("2020-02-10")) %>%
+   filter(name == "Reff") %>%
+# ugly way to calculate 7 day smoothing 
+   arrange(county, paramset, date) %>%
+   group_by(county, paramset) %>%
+   mutate(
+     lag3 = lag(mid, 3),
+     lag2 = lag(mid, 2),
+     lag1 = lag(mid, 1),
+     lead1 = lead(mid, 1),
+     lead2 = lead(mid, 2),
+     lead3 = lead(mid, 3)
+   ) %>% 
+   rowwise() %>% 
+   mutate(mid = mean(c(lag1, lag2, lag3, mid, lead1, lead2, lead3), na.rm = T)) %>% 
+   select(-starts_with("lag"), -starts_with("lead")) %>% 
+   mutate(paramset = as.character(paramset)) %>%
+   group_by(county, state, date) %>% 
+   {rbind(., 
+          dplyr::summarise(., 
+                           med = mean(mid), 
+                           lwr = min(mid),
+                           upr = max(mid),
+                           .groups = "drop") %>% 
+            rename(mid = med) %>% 
+            mutate(paramset = "summary",
+                   name = "Detect",
+                   intervention = "Reality",
+                   data = NA))} %>% 
+   mutate(width = ifelse(paramset == "summary", 1.5, 0.0),
+          alpha = ifelse(paramset == "summary", 1, 0.0)) %>%
+   ggplot(aes(x = date, y = mid, ymin = lwr, ymax = upr, 
+              fill  = county, color = county, 
+              group = interaction(county, paramset))) +
+   geom_ribbon(alpha = 0.25, color = NA) +
+   geom_line(aes(size = I(width), alpha = I(alpha))) +
+   scale_color_manual(guide = F, values = fig1_colors[c(1, 2, 5, 3, 4)]) +
+   scale_fill_manual(guide  = F, values = fig1_colors[c(1, 2, 5, 3, 4)]) +
+   geom_hline(yintercept    = 1, linetype = "dashed") + 
+   ylab(expression("R"[e])) + 
+   xlab("") +
+   scale_y_continuous(position = "left") + 
+ #  labs(tag = "B.") +
+   ggtitle("Reproduction number") +
+   theme(plot.title = element_text(face = "bold")
+      , plot.margin = unit(c(0.00,2.6,0.00,2.01), "cm"))
+     # , plot.tag.position = "topleft"
+    #  , plot.tag = element_text(face = "bold"
+    #    ))
+   }
+ 
+# gridExtra::grid.arrange(fig1.1, fig1.2, ncol = 2, widths = c(4, 1.2))
+
+fig1 <- gridExtra::arrangeGrob(fig1.1, fig1.2, fig1.3,  
+                        layout_matrix = matrix(c(1, 1, 2, 3, 3, 3), 
+                                               byrow  = T, nrow = 2),
+                        widths = c(2.6, 1.3, 1.3), heights = c(2.5, 1.5)) 
+
+  gridExtra::grid.arrange(fig1)
+  ggsave("figures/Manuscript2/Figure1.pdf", 
+         fig1, 
+         device = "pdf",
+         width = 12,
+         height = 8)
+}
